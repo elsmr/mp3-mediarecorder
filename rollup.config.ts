2b6c5405e5d1784a619564cb9dd79c4fc0b0cd07
Elias Meire <elias@meire.dev>
2021-06-10 11:59:09 +0200

    fix(types): Include types folder in npm package

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,14 +1,15 @@
 import babel from '@rollup/plugin-babel';
 import resolve from '@rollup/plugin-node-resolve';
-import typescript from 'rollup-plugin-typescript2';
+import typescript from '@rollup/plugin-typescript';
 
 const pkg = require('./package.json');
-const basePlugins = [typescript({ useTsconfigDeclarationDir: true, clean: true }), resolve()];
+const basePlugins = [resolve(), typescript({ declaration: true, declarationDir: 'dist' })];
 const pluginsWithTranspile = [...basePlugins, babel()];
+const workerPlugins = [resolve(), typescript({ declaration: true, declarationDir: './' })];
+const workerPluginsWithTranspile = [...workerPlugins, babel()];
 const baseOutputOptions = {
     compact: true,
     interop: false,
-    sourcemap: true,
 };
 
 export default [
@@ -17,9 +18,9 @@ export default [
         output: [
             {
                 ...baseOutputOptions,
-                file: pkg.es2015,
+                entryFileNames: pkg.es2015,
+                dir: './',
                 format: 'es',
-                sourcemap: true,
             },
         ],
         plugins: basePlugins,
@@ -29,17 +30,20 @@ export default [
         output: [
             {
                 ...baseOutputOptions,
-                file: pkg.module,
+                entryFileNames: pkg.module,
+                dir: './',
                 format: 'es',
             },
             {
                 ...baseOutputOptions,
-                file: pkg.main,
+                entryFileNames: pkg.main,
+                dir: './',
                 format: 'cjs',
             },
             {
                 ...baseOutputOptions,
-                file: pkg.browser,
+                entryFileNames: pkg.browser,
+                dir: './',
                 format: 'umd',
                 name: 'mp3MediaRecorder',
             },
@@ -51,32 +55,36 @@ export default [
         output: [
             {
                 ...baseOutputOptions,
-                file: 'dist/worker/index.es.js',
+                entryFileNames: 'dist/worker/index.es.js',
+                dir: './',
                 format: 'es',
             },
         ],
-        plugins: basePlugins,
+        plugins: workerPlugins,
     },
     {
         input: 'src/worker/index.ts',
         output: [
             {
                 ...baseOutputOptions,
-                file: 'dist/worker/index.es5.js',
+                entryFileNames: 'dist/worker/index.es5.js',
+                dir: './',
                 format: 'es',
             },
             {
                 ...baseOutputOptions,
-                file: 'dist/worker/index.js',
+                entryFileNames: 'dist/worker/index.js',
+                dir: './',
                 format: 'cjs',
             },
             {
                 ...baseOutputOptions,
-                file: 'dist/worker/index.umd.js',
+                entryFileNames: 'dist/worker/index.umd.js',
+                dir: './',
                 format: 'umd',
                 name: 'mp3EncoderWorker',
             },
         ],
-        plugins: pluginsWithTranspile,
+        plugins: workerPluginsWithTranspile,
     },
 ];
